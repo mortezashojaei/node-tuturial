9d36857d7bb69e7f000021edad964ed5083dbc30
morteza shojaei <mshojaei179@gmail.com>
2020-07-10 17:50:32 +0430

    setup mongodb configuration

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production"){
+    require("dotenv").config()
+}
+
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
@@ -11,5 +15,11 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use("/", indexRouter);
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected to mongoose !"));
 
 app.listen(process.env.PORT || 3000);
