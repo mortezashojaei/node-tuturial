87bd1b9030dabfe84d8769240ac46498be736817
morteza shojaei <mshojaei179@gmail.com>
2020-07-10 17:31:29 +0430

    set up template engine

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
+const indexRouter = require("./routes/index");
+
 app.set("view engine", "ejs");
-app.set("ejs", `${__dirname}/views`);
+app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use("/", indexRouter);
 
 app.listen(process.env.PORT || 3000);
